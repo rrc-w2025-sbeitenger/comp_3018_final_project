2450a018b9e73f16da06a03c11b84aa9ba884200
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-03 22:32:01 -0500

    feat: Added HTTPS_STATUS object for different status codes. update: Using express.json() middleware to parse json request, and added router handler for the API.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express, { Express } from "express";
+import routes from "./api/v1/routes/routes";
 
 const app: Express = express();
+
+//global middleware.
+app.use(express.json());
+//app.use((morgan("combined")));
+
+//router handler for marathon API.
+app.use("/api/v1", routes);
 
 export default app;
