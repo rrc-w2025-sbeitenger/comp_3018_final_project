3d07f80782462ea5dd83f0dba9e474908bc1fb48
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-05 02:32:22 -0500

    feat: add controller for getAllShells to call service logic.

--- src/api/v1/controllers/controllers.ts
+++ src/api/v1/controllers/controllers.ts
@@ -2,12 +2,23 @@ import { HTTP_STATUS } from "../../../constants/httpsConstants";
 import { Request, Response }from "express";
 import { successResponse } from "../models/responseModel"; 
 import { HealthCheckResponse } from "../models/healthCheckResponse";
-import { getHealthStatusService } from "../services/services";
+import { Shell } from "../models/shellModel";
+import { getHealthStatusService,
+          getAllShellsService} from "../services/services";
 
 export const getHealthCheck = (req: Request, res: Response): void => {
     try{
         const healthStatus: HealthCheckResponse = getHealthStatusService();
         res.status(HTTP_STATUS.OK).json(successResponse(healthStatus));
+    } catch (error){
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"});
+    }
+}
+
+export const getAllShells = async (req: Request, res:Response): Promise<void> => {
+    try {
+        const getAllShellsResult: Shell[] = await getAllShellsService();
+        res.status(HTTP_STATUS.OK).json(successResponse(getAllShellsResult));
     } catch (error){
         res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"});
     }
