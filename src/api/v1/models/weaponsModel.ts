b95d6bde86c70bcabc0ad55533ed48c490f149c2
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-05 02:46:16 -0500

    feat: Add Weapons interface to match database schema.

---
+++ src/api/v1/models/weaponsModel.ts
@@ -0,0 +1,11 @@
+//! add swagger docs
+export interface Weapons {
+    damage: number,
+    precision_multiplier: number,
+    rate_of_fire: string,
+    ads_speed: string,
+    equip_speed: string,
+    reload_speed: string,
+    recoil: string, 
+    aim_assist: number,
+}
