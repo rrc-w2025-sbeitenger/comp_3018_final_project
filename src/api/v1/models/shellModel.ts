a6f88ee16afc7eb1ddeeb6b1e179cfc78a4384de
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-05 02:27:26 -0500

    feat: Added Shell interface to match database schema

---
+++ src/api/v1/models/shellModel.ts
@@ -0,0 +1,20 @@
+//! add swagger docs later
+export interface Shell{
+    prime: string,
+    tactical: string,
+    trait_1: string,
+    trait_2: string, 
+    heat_capacity: number,
+    agility: number,
+    loot_speed: number,
+    melee_damage: number,
+    prime_recovery: number,
+    tactical_recovery: number,
+    self_repair_speed: number,
+    finisher_siphon: number,
+    revive_speed: number,
+    hardware: number,
+    firewall: number,
+    fall_resistance: number,
+    ping_duration: number
+}
