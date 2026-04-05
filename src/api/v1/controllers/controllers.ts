f7eb01d38c9190ba7b90ceb7be37932bdef4f3db
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-05 02:55:49 -0500

    feat: Add controller for getAllFactions to call service logic.

--- src/api/v1/controllers/controllers.ts
+++ src/api/v1/controllers/controllers.ts
@@ -4,9 +4,11 @@ import { successResponse } from "../models/responseModel";
 import { HealthCheckResponse } from "../models/healthCheckResponse";
 import { Shell } from "../models/shellModel";
 import { Weapons } from "../models/weaponsModel";
+import { Factions } from "../models/factionsModel";
 import { getHealthStatusService,
           getAllShellsService,
-           getAllWeaponsService} from "../services/services";
+           getAllWeaponsService,
+            getAllFactionsService} from "../services/services";
 
 export const getHealthCheck = (req: Request, res: Response): void => {
     try{
@@ -30,6 +32,15 @@ export const getAllWeapons = async (req: Request, res:Response): Promise<void> =
     try {
         const getAllWeaponsResult: Weapons[] = await getAllWeaponsService();
         res.status(HTTP_STATUS.OK).json(successResponse(getAllWeaponsResult));
+    } catch (error){
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"});
+    }
+}
+
+export const getAllFactions = async (req: Request, res:Response): Promise<void> => {
+    try {
+        const getAllFactionsResult: Factions[] = await getAllFactionsService();
+        res.status(HTTP_STATUS.OK).json(successResponse(getAllFactionsResult));
     } catch (error){
         res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"});
     }
