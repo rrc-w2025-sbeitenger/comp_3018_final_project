fe702fc3fe3abe0f4b863cc63e9e27fc94538cc1
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-05 22:39:44 -0500

    docs: Add swagger/openapi documention to describe Factions interface.

--- src/api/v1/models/factionsModel.ts
+++ src/api/v1/models/factionsModel.ts
@@ -1,3 +1,25 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Factions:
+ *       type: object
+ *       required:
+ *         - name
+ *         - lore
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the faction
+ *           example: "CYBERACME"
+ *         lore:
+ *           type: string
+ *           description: The lore and background of the faction
+ *           example: "The industry leader in AI."
+ *       example:
+ *         name: "CYBERACME"
+ *         lore: "The industry leader in AI."
+ */
 export interface Factions {
     name: string,
     lore: string
