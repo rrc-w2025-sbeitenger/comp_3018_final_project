9d0084db0b092b98b1c7431ba2da0e2a9de201a5
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-04-09 13:20:30 -0500

    update: Add logic to service layer for all getByName methods for shells, factions, and weapons.

--- src/api/v1/services/services.ts
+++ src/api/v1/services/services.ts
@@ -31,15 +31,63 @@ export const getAllFactionsService = async (): Promise<Factions[]> => {
 }
 
 export const getShellByNameService = async (shellName: string): Promise<Shell | false> => {
-    
+    const entity: Shell | false = await getShellById(shellName);
+
+    if(!entity){
+        return false;
+    } else {
+        return {
+            prime: entity.prime,
+            tactical: entity.tactical,
+            trait_1: entity.trait_1,
+            trait_2: entity.trait_2, 
+            heat_capacity: entity.heat_capacity,
+            agility: entity.agility,
+            loot_speed: entity.loot_speed,
+            melee_damage: entity.melee_damage,
+            prime_recovery: entity.prime_recovery,
+            tactical_recovery: entity.tactical_recovery,
+            self_repair_speed: entity.self_repair_speed,
+            finisher_siphon: entity.finisher_siphon,
+            revive_speed: entity.revive_speed,
+            hardware: entity.hardware,
+            firewall: entity.firewall,
+            fall_resistance: entity.fall_resistance,
+            ping_duration: entity.ping_duration
+        }
+    }
 }
 
 export const getWeaponByNameService = async (weaponName: string): Promise<Weapons | false> => {
-    
+    const entity: Weapons | false = await getWeaponById(weaponName);
+
+    if(!entity){
+        return false;
+    } else {
+        return {
+            damage: entity.damage,
+            precision_multiplier: entity.precision_multiplier,
+            rate_of_fire: entity.rate_of_fire,
+            ads_speed: entity.ads_speed,
+            equip_speed: entity.equip_speed,
+            reload_speed: entity.reload_speed,
+            recoil: entity.recoil, 
+            aim_assist: entity.aim_assist,
+        }
+    }
 }
 
-export const getFactionByNameService = (factionName: string): any => {
-    console.log("faction by name");
+export const getFactionByNameService = async (factionName: string): Promise<Factions | false> => {
+    const entity: Factions | false = await getFactionById(factionName);
+
+    if(!entity){
+        return false;
+    } else {
+        return {
+            name: entity.name,
+            lore: entity.lore,
+        }
+    }
 }
 
 export const createShellService = (prime: string,
